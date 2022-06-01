ae75509e892f3cb731bcc4c9afed4fd9b50a36e8
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-01 12:34:42 +0700

    Create idlast0.js

---
+++ b/idlast0.js
@@ -0,0 +1,28 @@
+/* TITLE */
+doc.title=idSetting.judul;
+
+/* HEADING 1 */
+let heading1=doc.createElement('h1');
+heading1.innerHTML=idSetting.judul;
+kmain.insertBefore(heading1,kmain.firstElementChild);
+
+/* CONTENT */
+let divContent=doc.createElement('div');
+divContent.innerHTML=idContent.join('');
+kmain.appendChild(divContent);
+
+/* META INDEX */
+let metaRobots=doc.createElement('meta');
+metaRobots.name='robots';
+if (idSetting.index === !0) {
+  metaRobots.content='index, follow';
+} else {
+  metaRobots.content='noindex, nofollow';
+}
+doc.head.appendChild(metaRobots);
+
+/* META DESCRIPTION */
+let metaDescription=doc.createElement('meta');
+metaDescription.name='description';
+metaDescription.content=idSetting.deskripsi;
+doc.head.appendChild(metaDescription);
