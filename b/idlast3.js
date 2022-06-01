ac3b77d71260777871010f61d6c05188fedaf68c
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-01 15:42:37 +0700

    Update and rename idlast2.js to idlast3.js

--- b/idlast3.js
+++ b/idlast3.js
@@ -1,8 +1,16 @@
 if (typeof idSetting === 'undefined') {
+/* META NOINDEX */
 let metaRobots=doc.createElement('meta');
 metaRobots.name='robots';
 metaRobots.content='noindex, nofollow';
 doc.head.appendChild(metaRobots);
+
+/* META REFRESH */
+let metaRefresh=doc.createElement('meta');
+metaRefresh.setAttribute('http-equiv', 'refresh');
+metaRefresh.content='0; url='+tls+hn+'/404';
+doc.head.appendChild(metaRefresh);
+
 }
 
 if (typeof idSetting !== 'undefined') {
