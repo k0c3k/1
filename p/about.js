df578f8016ffbc381a827f62a4f0ff6efb43b801
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-01 12:29:45 +0700

    Create about.js

---
+++ p/about.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: true,
+  label: 'Page',
+  judul: 'Tentang Kocek.co',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=about'
+};
+var idContent=[
+'<p>Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.</p>'
+];
