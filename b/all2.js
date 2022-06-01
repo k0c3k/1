3ee7ba7b7e5fc1a88e9986029debcf19f7dea6d8
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-01 18:34:04 +0700

    Update and rename all1.js to all2.js

--- b/all2.js
+++ b/all2.js
@@ -14,7 +14,7 @@ inAfter(topinfo,kheader);
 
 /* Footer */
 var kfoot1='<div class="foot-info"><h2><i class="icon-xor"></i>Kocek.co</h2><h3>Situs Finansial Digital Kreatif</h3><ul><li><a href="/id?p=about">Apa itu Kocek.co?</a></li><li><a href="#">Cara Pembayaran</a></li><li><a href="#">Biaya Layanan</a></li><li><a href="#" rel="nofollow noopener">Syarat dan Ketentuan</a></li><li><a href="#" rel="nofollow noopener">Kebijakan Privasi</a></li><li><a href="#">FAQ</a></li></ul><p data-nosnippet>Saat ini Kocek.co merupakan personal business yang tidak berafiliasi dengan platform manapun.</p></div>',
-    kfoot2 = '<div class="ongoogle"><a href="https://www.google.com"><b>Search on</b><i class="svg-google"></i></a></div>';
+    kfoot2 = '<div class="ongoogle"><a href="https://www.google.com" rel="nofollow noopener" target="_blank"><b>Search on</b><i class="svg-google"></i></a></div>';
 
 let topfoot=doc.createElement('div');
 topfoot.innerHTML=kfoot1+kfoot2;
