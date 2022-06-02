30b312b22267777a709f331cd0ef0c1deae1fab0
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 09:56:08 +0700

    Create kebijakan-privasi0.js

---
+++ p/kebijakan-privasi0.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: false,
+  label: 'Page',
+  judul: 'Kebijakan Privasi',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=about'
+};
+var idContent=[
+'<p>Kebijakan Privasi akan segera diatur.</p>'
+];
