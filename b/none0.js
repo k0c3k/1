44cef95a2a6301d119f1c5bc7924c8d0043904ac
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 10:53:10 +0700

    Update none0.js

--- b/none0.js
+++ b/none0.js
@@ -1,6 +1,11 @@
-
 /* NOINDEX */
 let metaRobots=doc.createElement('meta');
 metaRobots.name='robots';
 metaRobots.content='noindex, nofollow';
 doc.head.appendChild(metaRobots);
+
+/* META REFRESH */
+let metaRefresh=doc.createElement('meta');
+metaRefresh.setAttribute('http-equiv', 'refresh');
+metaRefresh.content='0; url='+tls+hn+'/404';
+doc.head.appendChild(metaRefresh);
