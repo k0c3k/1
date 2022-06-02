d15b9bb4362ae0f7919301ab26f7ee67dc360fba
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 10:22:12 +0700

    Create biaya0.js

---
+++ p/biaya0.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: true,
+  label: 'Page',
+  judul: 'Biaya Layanan',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=biaya'
+};
+var idContent=[
+'<p>Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.</p>'
+];
