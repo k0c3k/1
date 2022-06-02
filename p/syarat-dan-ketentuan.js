64b69bd30283ca4acebcd056807e3e9e1dedd741
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 09:52:48 +0700

    Create syarat-dan-ketentuan.js

---
+++ p/syarat-dan-ketentuan.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: false,
+  label: 'Page',
+  judul: 'Syarat dan Ketentuan',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=syarat-dan-ketentuan'
+};
+var idContent=[
+'<p>Dengan menggunakan Kocek pengguna telah setuju dengan syarat dan ketentuan.</p>'
+];
