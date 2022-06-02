01efca66b79b4f05f0c54d48374896ca3e39f2dd
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 10:17:25 +0700

    Create cara-pembayaran0.js

---
+++ p/cara-pembayaran0.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: true,
+  label: 'Page',
+  judul: 'Cara Pembayaran',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=cara-pembayaran'
+};
+var idContent=[
+'<p>Cara pembayaran.</p>'
+];
