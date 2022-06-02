8cb8990e47c10b00eb4e89905fcf23155c060613
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 10:19:48 +0700

    Create faq0.js

---
+++ p/faq0.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: true,
+  label: 'Page',
+  judul: 'FAQ',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=faq'
+};
+var idContent=[
+'<p>Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.</p>'
+];
