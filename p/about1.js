23bbf7ee434af80aa177509e76c8ca2123dc21cc
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-07 06:32:43 +0700

    Update and rename about0.js to about1.js

--- p/about1.js
+++ p/about1.js
@@ -1,6 +1,5 @@
 var idSetting={
   index: true,
-  label: 'Page',
   judul: 'Tentang Kocek.co',
   deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
   permalink: '/id?p=about'
