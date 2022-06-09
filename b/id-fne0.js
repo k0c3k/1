f295bf86d9501c99c177922c59577e08bfa8fcf5
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-09 10:37:06 +0700

    Create id-fne0.js

---
+++ b/id-fne0.js
@@ -0,0 +1,24 @@
+function fne(f,n,e){
+  var s=doc.createElement('script');
+    s.async=!1;
+    s.crossOrigin='anonymous';
+    s.setAttribute('data-id','fne');
+    s.src=tls+kk1+f+'/'+n+e+'.min.js'+ver;
+    s.type='text/javascript';
+  if(typeof s!=='undefined' && lSearch==='?'+f+'='+n){
+    kbody.appendChild(s);
+    s.addEventListener('load',function(){
+      var id=doc.querySelector('[data-id="fne"]');
+      if(id!==null){
+        loadynk('b/exist0','b');
+      }
+    })
+  }
+};
+
+function fneLast(){
+  var id=doc.querySelector('[data-id="fne"]');
+  if(id===null){
+    loadynk('b/none','b');
+  }
+};
