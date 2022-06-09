7d00ce94559530a080846f68ed46ae08bc995f6d
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-09 15:33:40 +0700

    Create layanan0.js

---
+++ p/layanan0.js
@@ -0,0 +1,10 @@
+var idSetting={
+  index: true,
+  judul: 'Layanan Kocek.co',
+  deskripsi: 'Kocek.co adalah situs yang menghadirkan informasi serta layanan finansial digital kreatif.',
+  permalink: '/id?p=layanan'
+};
+var idContent=[
+'<p>Saat ini Kocek memiliki layanan berikut:</p>',
+'<ul class="hyphen-minus"><li>Kocek Transfer</li><li>Kocek Top Up</li></ul>'
+];
