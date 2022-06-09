f7583ac1641da4e20109a98153d4e1009569886f
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-09 10:01:57 +0700

    Update klast0.js

--- b/klast0.js
+++ b/klast0.js
@@ -1,13 +1,15 @@
-
+for(let i=0;i<kimgAll.length;i++){
+  kimgAll[i].oncontextmenu=function(){return false};
+}
 if ('loading' in HTMLImageElement.prototype) {
-  let i = document.querySelectorAll('img[loading="lazy"]');
+  let i = doc.querySelectorAll('img[loading="lazy"]');
   i.forEach(img => {
     img.src = img.dataset.src;
   });
 } else {
-  let s = document.createElement('script');
+  let s=doc.createElement('script');
   s.type='text/javascript';
   s.crossOrigin='anonymous';
-  s.src = tls+'cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/17.3.1/lazyload.min.js';
-  doc.body.appendChild(s);
+  s.src=tls+'cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/17.3.1/lazyload.min.js';
+  kbody.appendChild(s);
 }
