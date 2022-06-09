d2a035d9fc4d72719b218c15489f3f19c25ecb7a
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-09 09:57:02 +0700

    Update idlast6.js

--- b/idlast6.js
+++ b/idlast6.js
@@ -1,20 +1,4 @@
-if (typeof idSetting === 'undefined') {
-
-/* META NOINDEX */
-let metaRobots=doc.createElement('meta');
-metaRobots.name='robots';
-metaRobots.content='noindex, nofollow';
-doc.head.appendChild(metaRobots);
-
-/* META REFRESH */
-let metaRefresh=doc.createElement('meta');
-metaRefresh.setAttribute('http-equiv', 'refresh');
-metaRefresh.content='0; url='+tls+hn+'/404';
-doc.head.appendChild(metaRefresh);
-
-}
-
-if (typeof idSetting !== 'undefined') {
+if (typeof idSetting!=='undefined'){
 
 /* TITLE */
 doc.title=idSetting.judul;
@@ -25,45 +9,45 @@ let article=doc.createElement('article');
 article.innerHTML=h1+idContent.join('');
 kmain.appendChild(article);
 
-let karticle=doc.querySelector("article");
-
-let breadcrumb=doc.createElement('ul');
-breadcrumb.className='breadcrumb';
-if (typeof idSetting.label !== 'undefined') {
-breadcrumb.innerHTML='<li><a href="'+tls+hn+'">Beranda</a></li><li><a href="'+idSetting.label.url+'">'+idSetting.label.name+'</a></li>';
+/* BREADCRUMB */
+var breadcrumb1=doc.createElement('ul');
+breadcrumb1.className='breadcrumb';
+if(typeof idSetting.label!=='undefined'){
+  breadcrumb1.innerHTML='<li><a href="'+tls+kk+'">Beranda</a></li><li><a href="'+idSetting.label.url+'">'+idSetting.label.name+'</a></li>';
 } else {
-breadcrumb.innerHTML='<li><a href="'+tls+hn+'">Beranda</a></li>';
+  breadcrumb1.innerHTML='<li><a href="'+tls+kk+'">Beranda</a></li>';
 };
-karticle.insertBefore(breadcrumb,karticle.firstElementChild);
+let karticle=doc.querySelector('article');
+karticle.insertBefore(breadcrumb1,karticle.firstElementChild);
 
 /* META INDEX */
-let metaRobots=doc.createElement('meta');
+var metaRobots=doc.createElement('meta');
 metaRobots.name='robots';
-if (idSetting.index === !0) {
+if (idSetting.index===!0){
   metaRobots.content='index, follow';
 } else {
   metaRobots.content='noindex, nofollow';
 }
-doc.head.appendChild(metaRobots);
+khead.appendChild(metaRobots);
 
 /* META DESCRIPTION */
-let metaDescription=doc.createElement('meta');
-metaDescription.name='description';
-metaDescription.content=idSetting.deskripsi;
-doc.head.appendChild(metaDescription);
+let metaDesc=doc.createElement('meta');
+metaDesc.name='description';
+metaDesc.content=idSetting.deskripsi;
+khead.appendChild(metaDesc);
 
 /* CANONICAL */
 let linkCanonical=doc.createElement('link');
 linkCanonical.rel='canonical';
-linkCanonical.href=tls+hn+idSetting.permalink;
-doc.head.appendChild(linkCanonical);
+linkCanonical.href=tls+kk+idSetting.permalink;
+khead.appendChild(linkCanonical);
 
 /* BREADCRUMBLIST */
-if (typeof idSetting.label !== 'undefined') {
-let breadcrumbList=doc.createElement('script');
-breadcrumbList.setAttribute('type' ,'application/ld+json');
-breadcrumbList.textContent='{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"name":"Beranda","item":"'+tls+hn+'"},{"@type":"ListItem","position":2,"name":"'+idSetting.label.name+'","item":"'+tls+hn+idSetting.label.url+'"}]}';
-doc.head.appendChild(breadcrumbList);
+if (typeof idSetting.label!=='undefined'){
+  let breadcrumbList=doc.createElement('script');
+  breadcrumbList.setAttribute('type' ,'application/ld+json');
+  breadcrumbList.textContent='{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"name":"Beranda","item":"'+tls+kk+'"},{"@type":"ListItem","position":2,"name":"'+idSetting.label.name+'","item":"'+tls+kk+idSetting.label.url+'"}]}';
+  khead.appendChild(breadcrumbList);
 }
 
 }
